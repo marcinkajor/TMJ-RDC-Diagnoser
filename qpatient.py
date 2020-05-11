9318660b3bfb255558587fc896bf34771444e780
Marcin <marcinkajor@gmail.com>
2020-05-11 23:20:03 +0200

    Implemented first part of the Axis 1 Group II algorithm (left side of the diagram).

--- qpatient.py
+++ qpatient.py
@@ -18,6 +18,7 @@ class Person:
                 self.name, self.surname, self.age, self.sex)
 
 class Patient:
+    LIMIT_DIFF_E6 = 5 #mm
     def __init__(self, idx, personalData, axisOne, palpations, q):
         self.idx = idx
         self.personalData = personalData
@@ -27,33 +28,59 @@ class Patient:
     def getAsixI1Diagnosis(self):
         q3 = self.q.getQ3()
         if (q3 == 0):
-            return "No pain"
+            return "No group Idx"
         else:
             left = self.palpations["E9"].painScore("left") + self.palpations["E11"].painScore("left")
             right = self.palpations["E9"].painScore("right") + self.palpations["E11"].painScore("right")
             if ((left + right) < 3):
-                return "No pain"
+                return "No group Idx"
             else:
                 painSourceNumeric, painSide = self.axisOne.E2.getPainSource()
                 if (not ((left and painSide in ["left", "both"]) or (right and painSide in ["right", "both"]))):
-                    return "No Pain"
+                    return "No group Idx"
                 else:
                     painFreeOpen = self.axisOne.E5.getOpening("E5a") + self.axisOne.E5.getOpening("E5d")
                     if (painFreeOpen >= 40):
-                        return "Ia"
+                        return "Ia Myofascial Pain"
                     else:
                         passiveStretch = self.axisOne.E5.getOpening("E5c") + self.axisOne.E5.getOpening("E5a")
                         if (passiveStretch < 5):
-                            return "Ia"
+                            return "Ia Myofascial Pain"
                         else:
-                            return "Ib"
+                            return "Ib Myofascial Pain with limited opening"
     def getAxisI2Diagnosis(self, side):
-        # TODO: Implement
+        opening = self.axisOne.E6.isSound(side, "open")
+        closing = self.axisOne.E6.isSound(side, "close")
+        if (opening and closing):
+            diff = self.axisOne.E6.getMeasure(side, "open") - self.axisOne.E6.getMeasure(side, "close")
+            if (diff >= LIMIT_DIFF_E6):
+                if (axisOne.E6.isClickElimination()):
+                    return "{} DD with reduction".format(side)
+                else:
+                    if (self.isE8Relevant()):
+                        return "{} DD with reduction".format(side)
+                    else:
+                        return self.historyDependentDiagnosis()
+            elif (self.isE8Relevant()):
+                return "{} DD with reduction".format(side)
+        elif (opening or closing):
+            if (self.isE8Relevantside()):
+                return "{} DD with reduction".format(side)
+            else:
+                return self.historyDependentDiagnosis()
+        else:
+            return self.historyDependentDiagnosis()
+
+    def historyDependentDiagnosis(self):
+        #TODO: implement
         pass
-        # opening = self.axisOne.E6.isSound(side, "open")
-        # closing = self.axisOne.E6.isSound(side, "close")
-        # if (opening and closing):
-        #     diff = self.axisOne.E6.
+
+    def isE8Relevant(self, side):
+        rightExcursion = self.axisOne.E8.isSound(side, "right")
+        leftExcursion = self.axisOne.E8.isSound(side, "left")
+        rightProtrusion = self.axisOne.E8.isSound("protrusion", "right")
+        leftProtrusion = self.axisOne.E8.isSound("protrusion", "left")
+        return (rightExcursion or leftExcursion or rightProtrusion or leftProtrusion)
 
 """ This function creates the whole final patients dictionary """
 def formPatientsDict(persons, axisOnes, palpations, qs):
