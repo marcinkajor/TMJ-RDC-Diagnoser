94c5996f3dc864839c814242ae132e2c64015624
Marcin <marcinkajor@gmail.com>
2020-05-26 23:04:21 +0200

    Opening examination file move to try-catch block. Added notifications when opening a file. Interface prepared for selecting a file to store diagnosis.

--- qgui.py
+++ qgui.py
@@ -5,7 +5,7 @@ Created on Tue May  5 19:49:43 2020
 @author: Marcin
 """
 
-from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QAction, QFileDialog
 import sys
 import pandas as pd
@@ -32,12 +32,17 @@ class Window(QMainWindow):
         openAction.setStatusTip('Quit the application')
         openAction.triggered.connect(QtWidgets.QApplication.quit)
 
+        generateDiagnosisAction = QAction("Generate diagnostic file", self)
+        generateDiagnosisAction.setStatusTip('Generate the diagnosis based on the the examination file')
+        generateDiagnosisAction.triggered.connect(self.generateDiagnosticReport)
+
         self.statusBar()
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('File')
         fileMenu.addAction(quitAction)
         fileMenu.addAction(openAction)
+        fileMenu.addAction(generateDiagnosisAction)
 
         self.show()
 
@@ -54,22 +59,34 @@ class Window(QMainWindow):
     def openDiagnosticFile(self):
         fileName, fileFilter = QFileDialog.getOpenFileName(self, 'Open File',
                                                filter="Excel files (*.xlsx)")
-        # Import datasets as separate spreadsheets
-        axis1_sheet = pd.read_excel('list.xlsx', sheet_name = 'axis I')
-        palpation_sheet = pd.read_excel('list.xlsx', sheet_name = 'axis I palpacja')
-        q_sheet = pd.read_excel('list.xlsx', sheet_name = 'Q')
 
-        # transform to np objects
-        axis1_data = removeEmpty(axis1_sheet.to_numpy())
-        palpation_data = removeEmpty(palpation_sheet.to_numpy())
-        q_data = removeEmpty(q_sheet.to_numpy())
+        try:
+            # Import datasets as separate spreadsheets
+            axis1_sheet = pd.read_excel(fileName, sheet_name = 'axis I')
+            palpation_sheet = pd.read_excel(fileName, sheet_name = 'axis I palpacja')
+            q_sheet = pd.read_excel(fileName, sheet_name = 'Q')
 
-        # assume that all sheets have the same number of records/patients
-        assert(len(axis1_data) == len(palpation_data) == len(q_data))
+            # transform to np objects
+            axis1_data = removeEmpty(axis1_sheet.to_numpy())
+            palpation_data = removeEmpty(palpation_sheet.to_numpy())
+            q_data = removeEmpty(q_sheet.to_numpy())
 
-        persons, axisOnes, palpations, qs = parseDatabase(axis1_data, palpation_data, q_data)
-        patients = formPatientsDict(persons, axisOnes, palpations, qs)
-        printDiagnosis(patients)
+            # assume that all sheets have the same number of records/patients
+            assert(len(axis1_data) == len(palpation_data) == len(q_data))
+
+            persons, axisOnes, palpations, qs = parseDatabase(axis1_data, palpation_data, q_data)
+            patients = formPatientsDict(persons, axisOnes, palpations, qs)
+            self.patients = patients
+            QMessageBox.question(self, "TMJ RDC Diagnoser", "File loaded properly",
+                                 QMessageBox.Ok, QMessageBox.Ok)
+            printDiagnosis(patients)
+        except:
+            QMessageBox.question(self, "TMJ RDC Diagnoser", "Wrong file format!",
+                     QMessageBox.Ok, QMessageBox.Ok)
+
+    def generateDiagnosticReport(self):
+        # TODO: implement
+        pass
 
 def run():
         app = QtWidgets.QApplication(sys.argv)
