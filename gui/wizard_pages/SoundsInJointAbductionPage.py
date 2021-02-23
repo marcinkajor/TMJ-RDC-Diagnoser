5921295c86492726ade61311901d0a235446a470
Marcin <marcinkajor@gmail.com>
2021-02-23 19:37:59 +0100

    Implemented doLoadWithData() method in SoundsInJointAbductionPage class.

--- gui/wizard_pages/SoundsInJointAbductionPage.py
+++ gui/wizard_pages/SoundsInJointAbductionPage.py
@@ -1,5 +1,6 @@
 from gui.wizard_pages.BaseWizardPage import BasePage
 from gui.wizard_pages.WizardPagesHelpers import *
+from algo.DatabaseDeserializer import DatabaseDeserializer
 
 
 class SoundsInJointAbductionPage(BasePage):
@@ -49,3 +50,43 @@ class SoundsInJointAbductionPage(BasePage):
     def clearAll(self):
         self.clickEliminationOptions.cleaAll()
         self.soundsOptions.cleaAll()
+
+    def doLoadWithData(self, patientId):
+        serializer = DatabaseDeserializer(self.wizard().getDatabase())
+        diagnosticData = serializer.getDiagnosticDataDictById(patientId)
+        soundsInJointAbductionData = diagnosticData["SoundsInJointAbduction"]
+
+        leftOpeningMm = soundsInJointAbductionData["left_opening_mm"]
+        leftClosingMm = soundsInJointAbductionData["left_closing_mm"]
+
+        rightOpeningMm = soundsInJointAbductionData["right_opening_mm"]
+        rightClosingMm = soundsInJointAbductionData["right_closing_mm"]
+
+        leftOpeningClickElimination = soundsInJointAbductionData["left_opening_click_elimination"]
+        leftClosingClickElimination = soundsInJointAbductionData["left_closing_click_elimination"]
+
+        rightOpeningClickElimination = soundsInJointAbductionData["right_opening_click_elimination"]
+        rightClosingClickElimination = soundsInJointAbductionData["right_closing_click_elimination"]
+
+        leftOpeningSound = soundsInJointAbductionData["left_opening_sound"]
+        leftClosingSound = soundsInJointAbductionData["left_closing_sound"]
+
+        rightOpeningSound = soundsInJointAbductionData["right_opening_sound"]
+        rightClosingSound = soundsInJointAbductionData["right_closing_sound"]
+
+        self.mm.getLineEdit("Left: opening").setText(leftOpeningMm)
+        self.mm.getLineEdit("Left: closing").setText(leftClosingMm)
+        self.mm.getLineEdit("Right: opening").setText(rightOpeningMm)
+        self.mm.getLineEdit("Right: closing").setText(rightClosingMm)
+
+        clickOptions = self.clickEliminationOptions.getOptions()
+        clickOptions["Left opening"].getButton(leftOpeningClickElimination).setChecked(True)
+        clickOptions["Left closing"].getButton(leftClosingClickElimination).setChecked(True)
+        clickOptions["Right opening"].getButton(rightOpeningClickElimination).setChecked(True)
+        clickOptions["Right closing"].getButton(rightClosingClickElimination).setChecked(True)
+
+        soundOptions = self.soundsOptions.getOptions()
+        soundOptions["Left opening"].getButton(leftOpeningSound).setChecked(True)
+        soundOptions["Left closing"].getButton(leftClosingSound).setChecked(True)
+        soundOptions["Right opening"].getButton(rightOpeningSound).setChecked(True)
+        soundOptions["Right closing"].getButton(rightClosingSound).setChecked(True)
