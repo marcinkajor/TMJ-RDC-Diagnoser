462315cc16b40ad37014e562a185893900403125
Marcin <marcinkajor@gmail.com>
2021-03-12 22:48:25 +0100

    Implemented audio playback feature.

--- audio/AudioHandler.py
+++ audio/AudioHandler.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtWidgets
-import winsound  # Note that this is Windows specific module
+from PyQt5.QtGui import QFont
 import numpy as np
+from scipy.io import wavfile
+import winsound
+import io
+import multiprocessing as multiprocess
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
@@ -29,10 +33,9 @@ class AudioSerializer(SaveFile):
         self.parent = parent
         self.icon = icon
 
-    def serialize(self, filePath: str, binaryData):
+    def serialize(self, filePath: str, fs, data):
         try:
-            with open(filePath, 'wb') as audioFile:
-                audioFile.write(binaryData)
+            wavfile.write(filePath, fs, data)
         except Exception as e:
             self.writeErrorNotification(e)
 
@@ -43,6 +46,17 @@ class PlotWidget(FigureCanvas):
         self.axes = self.fig.add_subplot(111)
         super(PlotWidget, self).__init__(self.fig)
         self.setParent(parent)
+
+# AudioManager class uses the winsound module which is Windows specific and makes the code not portable
+# the problem with platform independent audio modules is that they do not support 'weird' sampling rates
+# like 4k which is the case of audio signals recorded with Littmann stethoscope
+# To provide the non-blocking behaviour of the audio playback, the multiprocess module is used
+# as it facilitates immediate termination of the underlying process (not so simple when using threading)
+# The playAudio function must be global to be spawn in multiprocess.Process
+
+
+def playAudio(audio):
+    winsound.PlaySound(audio, winsound.SND_MEMORY)
 
 
 class AudioManager(SaveFile, QtWidgets.QWidget):
@@ -60,14 +74,29 @@ class AudioManager(SaveFile, QtWidgets.QWidget):
 
         self.fileName = filename
         self.label = QtWidgets.QLabel("Audio name")
+        font = QFont("Arial", 9)
+        font.setBold(True)
+        self.label.setFont(font)
         self.nameWidget = QtWidgets.QLineEdit()
         self.nameWidget.setText(self.fileName)
         self.nameWidget.setReadOnly(True)
-        self.binaryData = binaryData
         self.serializer = serializer
 
+        self.binaryData = binaryData
+        fs, signal = wavfile.read(io.BytesIO(binaryData))
+        self.signal = signal
+        self.fs = fs
+        try:
+            self.numberOfChannels = 2 if signal.shape[1] else 1
+        except Exception as e:
+            str(e)
+            self.numberOfChannels = 1
+
         self.plot = PlotWidget(self)
-        self.plot.axes.plot(np.frombuffer(self.binaryData, dtype='int16'))
+        tx = [x * 1 / self.fs for x in range(len(self.signal))]
+        # TODO: the self.normalizeData() causes significant delay when generating plots
+        self.plot.axes.plot(tx, self.signal)
+
         self.plot.axes.set_xlabel("Time [s]")
         self.plot.axes.set_ylabel("Amplitude")
         self.plot.draw()
@@ -87,26 +116,43 @@ class AudioManager(SaveFile, QtWidgets.QWidget):
     def _onSaveButtonClicked(self):
         try:
             path, fileFilter = QtWidgets.QFileDialog.getSaveFileName(self, 'Save file', filter="Audio file (*.wav)")
-            self.serializer.serialize(path, self.binaryData)
+            self.serializer.serialize(path, self.fs, self.signal)
         except Exception as e:
             print(e)
 
     def _onPlayButtonClicked(self):
+        try:
+            self.playbackProcess = multiprocess.Process(target=playAudio, args=(self.binaryData,))
+            self.playbackProcess.start()
+        except Exception as e:
+            print(e)
+
+    def _playAudio(self):
         winsound.PlaySound(self.binaryData, winsound.SND_MEMORY)
-        # TODO: replace with pyaudio to allow asynchronous playback
 
     def _onSavePlotButtonClicked(self):
         dpi, ok = QtWidgets.QInputDialog.getInt(self, "Choose the dpi", "dpi", 100, 100, 1000)
         try:
-            path, fileFilter = QtWidgets.QFileDialog.getSaveFileName(self, 'Save plot', filter="(*.png)")
             if ok:
+                path, fileFilter = QtWidgets.QFileDialog.getSaveFileName(self, 'Save plot', filter="(*.png)")
                 self.plot.fig.savefig(path, dpi=dpi)
             else:
-                print("Plotting with default dpi=500")
-                self.plot.fig.savefig(path, dpi=500)
+                self.writeErrorNotification("Cannot write with no dpi specified")
         except Exception as e:
             self.writeErrorNotification(e)
 
+    def _onStopPlayingButtonClicked(self):
+        try:
+            self.playbackProcess.terminate()
+        except Exception as e:
+            print(e)
+
     @staticmethod
-    def _onStopPlayingButtonClicked():
-        pass
+    def normalizeData(signal, norm_type='minus_one_one'):
+        sig_min = int(np.min(signal))
+        sig_max = int(np.max(signal))
+        max_min_diff = (sig_max - sig_min)
+        if norm_type == 'zero_one':
+            return [(signal[idx] - sig_min)/max_min_diff for idx in range(len(signal))]
+        elif norm_type == 'minus_one_one':
+            return [2*(signal[idx] - sig_min)/max_min_diff - 1 for idx in range(len(signal))]
